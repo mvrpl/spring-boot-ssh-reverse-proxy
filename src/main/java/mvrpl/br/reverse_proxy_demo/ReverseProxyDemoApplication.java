631967af3c378d25f40c372f063346cba8e3da00
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2025-10-05 18:19:44 -0300

    reverse proxy spring

---
+++ src/main/java/mvrpl/br/reverse_proxy_demo/ReverseProxyDemoApplication.java
@@ -0,0 +1,17 @@
+package mvrpl.br.reverse_proxy_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@ComponentScan({"mvrpl.br.controller"})
+@EnableAutoConfiguration
+@SpringBootApplication
+public class ReverseProxyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReverseProxyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mvrpl/br/reverse_proxy_demo/ReverseProxyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mvrpl.br.reverse_proxy_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReverseProxyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
