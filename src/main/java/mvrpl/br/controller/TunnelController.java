b1b5ee9c4d9bfe68819b7037733e08d217ffeaac
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2025-10-05 21:55:23 -0300

    check remote service online

--- src/main/java/mvrpl/br/controller/TunnelController.java
+++ src/main/java/mvrpl/br/controller/TunnelController.java
@@ -5,10 +5,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.*;
+import com.jcraft.jsch.JSchException;
 
 import java.io.IOException;
 import java.util.Collections;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 
 @RestController
 @RequestMapping("/tunnel")
@@ -18,6 +21,15 @@ public class TunnelController {
 
     public TunnelController(SshTunnelService sshTunnelService) {
         this.sshTunnelService = sshTunnelService;
+    }
+
+    public static boolean isPortOpen(String ipAddress, int port, int timeoutMillis) {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress(ipAddress, port), timeoutMillis);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
     }
 
     @RequestMapping(value = "/**", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
@@ -34,7 +46,14 @@ public class TunnelController {
                 headers.put(headerName, Collections.list(request.getHeaders(headerName)))
             );
 
-            return sshTunnelService.forwardRequest(path, method, body, headers, InetAddress.getByName("192.168.1.171"), 8087);
+            String targetIp = "192.168.1.171"; // IP do servidor remoto
+            int targetPort = 8087; // Porta do serviço no servidor remoto
+
+            if (!isPortOpen(targetIp, targetPort, 2000)) {
+                return new ResponseEntity<>("Serviço de destino indisponível no IP/Porta especificados.", HttpStatus.SERVICE_UNAVAILABLE);
+            }
+
+            return sshTunnelService.forwardRequest(path, method, body, headers, InetAddress.getByName(targetIp), targetPort);
         } catch (JSchException | IOException e) {
             e.printStackTrace();
             return new ResponseEntity<>("Falha ao estabelecer o túnel SSH: " + e.getMessage(), HttpStatus.SERVICE_UNAVAILABLE);
